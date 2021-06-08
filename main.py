23e7ffd7c4074ab360f4e28c60509fa1d4210acf
Tom Cummings <rex4343@hotmail.com>
2021-06-08 20:33:49 +0100

    Request processing partially enabled
    •Updates to the main.py file include:
        ◦Infinite while loop added to main.py.
        ◦main.py can search through the ‘FileSystem/HSData/RequestProcessing’ directory and filter for .json files.
        ◦It then reads the data and processes it to find the purpose.
    
    •Updates to the receiver.py file include:
        ◦/sendJsonData route added:
            ‣Gets the JSON data as a string from the incoming POST request.
            ‣Filters through each character in the string to see if it's a ‘.
            ‣If it is, it replaces it with a “.
            ‣If it's not it carries on.
            ‣This process is due to the fact that iOS shortcuts seems to use the wrong syntax when sending JSON data over a web request.
            ‣Gets the current date and time.
            ‣Creates a new .json file with the date and time as the name.
            ‣Writes the updated JSON data to the file.
            ‣Closes the file.
            ‣Returns “Data Received” to the request sender.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import telegram, json
+import telegram, json, os, time
 
 # Open all necessary JSON files.
 with open('keysAndHttpAddresses.json', 'r') as data_file:
@@ -8,3 +8,25 @@ bot = telegram.Bot(token = keysAndHttpAddresses["telegram-token"])
 
 def send_message(recepient_id, message):
     bot.sendMessage(chat_id = recepient_id, text = message)
+
+run = True
+
+while run:
+
+    # Request Processing System
+    # How it works:
+    # The output of the Flask Server and Telegram Bot are stored in .json files in the path 'FileSystem/HSData/RequestProcessing'.
+    # The main.py file, this file, then filters through that directory and finds the .json files, retrieves the data in them and processes it accordingly.
+
+	files = os.listdir('FileSystem/HSData/RequestProcessing')
+    
+	for file in files:
+		name, ext = os.path.splitext(file)
+		if ext == ".json":
+			data_file = open(f'FileSystem/HSData/RequestProcessing/{file}', 'r')
+			data = data_file.read()
+			data_file.close()
+			os.remove(f'FileSystem/HSData/RequestProcessing/{file}')
+		
+		else:
+			os.remove(f'FileSystem/HSData/RequestProcessing/{file}')
--- reciever.py
+++ reciever.py
@@ -1,5 +1,6 @@
 # File is used to recieve incoming web requests.
 
+import datetime
 from flask import Flask
 from flask import request
 
@@ -8,6 +9,22 @@ app = Flask(__name__)
 @app.route("/")
 def ping():
     return "Connection Succesful"
+
+@app.route("/sendData", methods=['POST'])
+def recieve_data():
+    data = str(request.json)
+
+    for c in data:
+        if c == "\'":
+            new_data = data.replace("\'", '\"')
+        else:
+            pass
+        
+    date = datetime.datetime.now()
+    file = open(f"FileSystem/HSData/RequestProcessing/{date}.json", 'w')
+    file.write(new_data)
+    file.close()
+    return 'Data Recieved'
 
 if __name__ == "__main__":
     app.run()
