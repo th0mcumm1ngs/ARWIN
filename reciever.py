11eec97a09c55a3389feaefce685fca3d44bf816
Tom Cummings <rex4343@hotmail.com>
2021-06-10 20:57:45 +0100

    Update receiver.py
    Added comments to explain code.

--- reciever.py
+++ reciever.py
@@ -6,24 +6,31 @@ from flask import request
 
 app = Flask(__name__)
 
+# Simple ping. Used by othere HomeSystem Syetems to check the system is online.
 @app.route("/")
 def ping():
     return "Connection Succesful"
 
 @app.route("/sendData", methods=['POST'])
 def recieve_data():
+    # Recieve the JSON data from the request.
     data = str(request.json)
 
+    # Change all single quotes to double quotes (Due to Apple Shortcuts Error).
     for c in data:
         if c == "\'":
             new_data = data.replace("\'", '\"')
         else:
             pass
-        
+
+    # Get the current date and time.
     date = datetime.datetime.now()
+    # Create file and set the date and time as the name.
     file = open(f"RequestProcessing/{date}.json", 'w')
+    # Write the JSON data to the file.
     file.write(new_data)
     file.close()
+    # Return confirmation message to the user.
     return 'Data Recieved'
 
 if __name__ == "__main__":
