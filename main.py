558fa91de49ab84d795fa79ec4baafef689d53fb
Tom Cummings <rex4343@hotmail.com>
2021-08-14 22:13:47 +0100

    Issue #12 Solved
    Added validation to check if there is at least one argument when commands are processed.

--- main.py
+++ main.py
@@ -91,30 +91,34 @@ while run:
 							with open('data.json', 'r') as data_file:
 								HS_Data = json.load(data_file)
 
-							# Used to stitch the words passed as arguments together.
-							words = 0
-							name = ""
+							if len(data["args"]) != 0:
+								# Used to stitch the words passed as arguments together.
+								words = 0
+								name = ""
 
-							for i in data["args"]:
-								if words >= 1:
-									name += " " + i
+								for i in data["args"]:
+									if words >= 1:
+										name += " " + i
+
+									else:
+										name += i
+									
+									words += 1
+
+								# Checks if a stopwatch with the same name already exists.
+								if name not in HS_Data["stopwatchesAndTimers"]:
+									HS_Data["stopwatchesAndTimers"][name] = str(datetime.datetime.now())
+
+									with open('data.json', 'w') as data_file:
+										json.dump(HS_Data, data_file, indent = 4)
+
+									functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully created. Type \"/checkstopwatch {name}\" to get the time since it was started.")
 
 								else:
-									name += i
-								
-								words += 1
-
-							# Checks if a stopwatch with the same name already exists.
-							if name not in HS_Data["stopwatchesAndTimers"]:
-								HS_Data["stopwatchesAndTimers"][name] = str(datetime.datetime.now())
-
-								with open('data.json', 'w') as data_file:
-									json.dump(HS_Data, data_file, indent = 4)
-
-								functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully created. Type \"/checkstopwatch {name}\" to get the time since it was started.")
+									functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name already exists.")
 
 							else:
-								functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name already exists.")
+								functions.send_message(recepient_id = data["chatID"], message = "Sorry, there seems to have been an error. If you need help with a command call '/help [COMMAND NAME]'.\n\nNote: Dont include the '/' of the command that you need assistance with in your command.")
 
 						new_stopwatch()
 					
@@ -124,196 +128,200 @@ while run:
 							with open('data.json', 'r') as data_file:
 								HS_Data = json.load(data_file)
 
-							# Used to stitch the words passed as arguments together.
-							words = 0
-							name = ""
+							if len(data["args"]) != 0:
+								# Used to stitch the words passed as arguments together.
+								words = 0
+								name = ""
 
-							for i in data["args"]:
-								if words >= 1:
-									name += " " + i
+								for i in data["args"]:
+									if words >= 1:
+										name += " " + i
+
+									else:
+										name += i
+									
+									words += 1
+
+								# Checks if the stopwatch exists.
+								if name in HS_Data["stopwatchesAndTimers"]:
+									time_started = datetime.datetime.strptime(str(HS_Data["stopwatchesAndTimers"][name]), "%Y-%m-%d %H:%M:%S.%f")
+
+									time_now = datetime.datetime.now()
+
+									time_elapsed = dateutil.relativedelta.relativedelta(time_now, time_started)
+
+									seconds = time_elapsed.seconds
+									
+									minutes = time_elapsed.minutes
+
+									hours = time_elapsed.hours
+									
+									days = time_elapsed.days
+
+									weeks = time_elapsed.weeks
+
+									years = time_elapsed.years
+
+									# Make sure all variables are whole numbers.
+									seconds = int(seconds)
+
+									minutes = int(minutes)
+
+									hours = int(hours)
+
+									days = int(days)
+
+									weeks = int(weeks)
+
+									years = int(years)
+
+									timeframes_used = 0
+									tf1 = None
+									tf2 = None
+									tf3 = None
+									tf4 = None
+									tf5 = None
+									tf6 = None
+
+									if years != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = years
+										elif tf2 == None:
+											tf2 = years
+										elif tf3 == None:
+											tf3 = years
+										elif tf4 == None:
+											tf4 = years
+										elif tf5 == None:
+											tf5 = years
+										elif tf6 == None:
+											tf6 = years
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+											
+									if weeks != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = weeks
+										elif tf2 == None:
+											tf2 = weeks
+										elif tf3 == None:
+											tf3 = weeks
+										elif tf4 == None:
+											tf4 = seconds
+										elif tf5 == None:
+											tf5 = weeks
+										elif tf6 == None:
+											tf6 = weeks
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+
+									if days != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = days
+										elif tf2 == None:
+											tf2 = days
+										elif tf3 == None:
+											tf3 = days
+										elif tf4 == None:
+											tf4 = days
+										elif tf5 == None:
+											tf5 = days
+										elif tf6 == None:
+											tf6 = days
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+
+									if hours != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = hours
+										elif tf2 == None:
+											tf2 = hours
+										elif tf3 == None:
+											tf3 = hours
+										elif tf4 == None:
+											tf4 = hours
+										elif tf5 == None:
+											tf5 = hours
+										elif tf6 == None:
+											tf6 = hours
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+
+									if minutes != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = minutes
+										elif tf2 == None:
+											tf2 = minutes
+										elif tf3 == None:
+											tf3 = minutes
+										elif tf4 == None:
+											tf4 = minutes
+										elif tf5 == None:
+											tf5 = minutes
+										elif tf6 == None:
+											tf6 = minutes
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+
+									if seconds != 0:
+										timeframes_used += 1
+										if tf1 == None:
+											tf1 = seconds
+										elif tf2 == None:
+											tf2 = seconds
+										elif tf3 == None:
+											tf3 = seconds
+										elif tf4 == None:
+											tf4 = seconds
+										elif tf5 == None:
+											tf5 = seconds
+										elif tf6 == None:
+											tf6 = seconds
+										else:
+											functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
+
+									if timeframes_used == 0:
+										functions.flagError(description = "The /checkstopwatch command failed: The timeframes_used variable ended at 0.")
+
+									text_templates = {
+										"1": f"It has been {tf1} seconds since the stopwatch was started",
+										"2": f"It has been {tf1} minutes and {tf2} seconds since the stopwatch was started",
+										"3": f"It has been {tf1} hours, {tf2} minutes and {tf3} seconds since the stopwatch was started",
+										"4": f"It has been {tf1} days, {tf2} hours, {tf3} minutes and {tf4} seconds since the stopwatch was started",
+										"5": f"It has been {tf1} weeks, {tf2} days, {tf3} hours, {tf4} minutes and {tf5} seconds since the stopwatch was started",
+										"6": f"It has been {tf1} years, {tf2} weeks, {tf3} days, {tf4} hours, {tf5} minutes and {tf6} seconds since the stopwatch was started"
+									}
+
+									if timeframes_used == 1:
+										text = text_templates["1"]
+
+									if timeframes_used == 2:
+										text = text_templates["2"]
+
+									if timeframes_used == 3:
+										text = text_templates["3"]
+
+									if timeframes_used == 4:
+										text = text_templates["4"]
+
+									if timeframes_used == 5:
+										text = text_templates["5"]
+
+									if timeframes_used == 6:
+										text = text_templates["6"]
+
+									functions.send_message(recepient_id = data["chatID"], message = text)
 
 								else:
-									name += i
-								
-								words += 1
-
-							# Checks if the stopwatch exists.
-							if name in HS_Data["stopwatchesAndTimers"]:
-								time_started = datetime.datetime.strptime(str(HS_Data["stopwatchesAndTimers"][name]), "%Y-%m-%d %H:%M:%S.%f")
-
-								time_now = datetime.datetime.now()
-
-								time_elapsed = dateutil.relativedelta.relativedelta(time_now, time_started)
-
-								seconds = time_elapsed.seconds
-								
-								minutes = time_elapsed.minutes
-
-								hours = time_elapsed.hours
-								
-								days = time_elapsed.days
-
-								weeks = time_elapsed.weeks
-
-								years = time_elapsed.years
-
-								# Make sure all variables are whole numbers.
-								seconds = int(seconds)
-
-								minutes = int(minutes)
-
-								hours = int(hours)
-
-								days = int(days)
-
-								weeks = int(weeks)
-
-								years = int(years)
-
-								timeframes_used = 0
-								tf1 = None
-								tf2 = None
-								tf3 = None
-								tf4 = None
-								tf5 = None
-								tf6 = None
-
-								if years != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = years
-									elif tf2 == None:
-										tf2 = years
-									elif tf3 == None:
-										tf3 = years
-									elif tf4 == None:
-										tf4 = years
-									elif tf5 == None:
-										tf5 = years
-									elif tf6 == None:
-										tf6 = years
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-										
-								if weeks != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = weeks
-									elif tf2 == None:
-										tf2 = weeks
-									elif tf3 == None:
-										tf3 = weeks
-									elif tf4 == None:
-										tf4 = seconds
-									elif tf5 == None:
-										tf5 = weeks
-									elif tf6 == None:
-										tf6 = weeks
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-
-								if days != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = days
-									elif tf2 == None:
-										tf2 = days
-									elif tf3 == None:
-										tf3 = days
-									elif tf4 == None:
-										tf4 = days
-									elif tf5 == None:
-										tf5 = days
-									elif tf6 == None:
-										tf6 = days
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-
-								if hours != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = hours
-									elif tf2 == None:
-										tf2 = hours
-									elif tf3 == None:
-										tf3 = hours
-									elif tf4 == None:
-										tf4 = hours
-									elif tf5 == None:
-										tf5 = hours
-									elif tf6 == None:
-										tf6 = hours
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-
-								if minutes != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = minutes
-									elif tf2 == None:
-										tf2 = minutes
-									elif tf3 == None:
-										tf3 = minutes
-									elif tf4 == None:
-										tf4 = minutes
-									elif tf5 == None:
-										tf5 = minutes
-									elif tf6 == None:
-										tf6 = minutes
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-
-								if seconds != 0:
-									timeframes_used += 1
-									if tf1 == None:
-										tf1 = seconds
-									elif tf2 == None:
-										tf2 = seconds
-									elif tf3 == None:
-										tf3 = seconds
-									elif tf4 == None:
-										tf4 = seconds
-									elif tf5 == None:
-										tf5 = seconds
-									elif tf6 == None:
-										tf6 = seconds
-									else:
-										functions.flagError(description = "The /checkstopwatch command failed: No 'tf' slots remaining.")
-
-								if timeframes_used == 0:
-									functions.flagError(description = "The /checkstopwatch command failed: The timeframes_used variable ended at 0.")
-
-								text_templates = {
-									"1": f"It has been {tf1} seconds since the stopwatch was started",
-									"2": f"It has been {tf1} minutes and {tf2} seconds since the stopwatch was started",
-									"3": f"It has been {tf1} hours, {tf2} minutes and {tf3} seconds since the stopwatch was started",
-									"4": f"It has been {tf1} days, {tf2} hours, {tf3} minutes and {tf4} seconds since the stopwatch was started",
-									"5": f"It has been {tf1} weeks, {tf2} days, {tf3} hours, {tf4} minutes and {tf5} seconds since the stopwatch was started",
-									"6": f"It has been {tf1} years, {tf2} weeks, {tf3} days, {tf4} hours, {tf5} minutes and {tf6} seconds since the stopwatch was started"
-								}
-
-								if timeframes_used == 1:
-									text = text_templates["1"]
-
-								if timeframes_used == 2:
-									text = text_templates["2"]
-
-								if timeframes_used == 3:
-									text = text_templates["3"]
-
-								if timeframes_used == 4:
-									text = text_templates["4"]
-
-								if timeframes_used == 5:
-									text = text_templates["5"]
-
-								if timeframes_used == 6:
-									text = text_templates["6"]
-
-								functions.send_message(recepient_id = data["chatID"], message = text)
+									functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
 
 							else:
-								functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
+								functions.send_message(recepient_id = data["chatID"], message = "Sorry, there seems to have been an error. If you need help with a command call '/help [COMMAND NAME]'.\n\nNote: Dont include the '/' of the command that you need assistance with in your command.")
 
 						check_stopwatch()
 
@@ -323,29 +331,33 @@ while run:
 							with open('data.json', 'r') as data_file:
 								HS_Data = json.load(data_file)
 
-							# Used to stitch the words passed as arguments together.
-							words = 0
-							name = ""
+							if len(data["args"]) != 0:
+								# Used to stitch the words passed as arguments together.
+								words = 0
+								name = ""
 
-							for i in data["args"]:
-								if words >= 1:
-									name += " " + i
+								for i in data["args"]:
+									if words >= 1:
+										name += " " + i
+
+									else:
+										name += i
+									
+									words += 1
+
+								if name in HS_Data["stopwatchesAndTimers"]:
+									HS_Data["stopwatchesAndTimers"][name] = str(datetime.datetime.now())
+
+									with open('data.json', 'w') as data_file:
+										json.dump(HS_Data, data_file, indent = 4)
+
+									functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully reset. Type \"/checkstopwatch {name}\" to get the time since it was started.")
 
 								else:
-									name += i
-								
-								words += 1
-
-							if name in HS_Data["stopwatchesAndTimers"]:
-								HS_Data["stopwatchesAndTimers"][name] = str(datetime.datetime.now())
-
-								with open('data.json', 'w') as data_file:
-									json.dump(HS_Data, data_file, indent = 4)
-
-								functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully reset. Type \"/checkstopwatch {name}\" to get the time since it was started.")
+									functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
 
 							else:
-								functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
+								functions.send_message(recepient_id = data["chatID"], message = "Sorry, there seems to have been an error. If you need help with a command call '/help [COMMAND NAME]'.\n\nNote: Dont include the '/' of the command that you need assistance with in your command.")
 
 						reset_stopwatch()
 
@@ -355,30 +367,34 @@ while run:
 							with open('data.json', 'r') as data_file:
 								HS_Data = json.load(data_file)
 
-							# Used to stitch the words passed as arguments together.
-							words = 0
-							name = ""
+							if len(data["args"]) != 0:
+								# Used to stitch the words passed as arguments together.
+								words = 0
+								name = ""
 
-							for i in data["args"]:
-								if words >= 1:
-									name += " " + i
+								for i in data["args"]:
+									if words >= 1:
+										name += " " + i
+
+									else:
+										name += i
+									
+									words += 1
+
+								# Checks if the stopwatch exists.
+								if name in HS_Data["stopwatchesAndTimers"]:
+									del HS_Data["stopwatchesAndTimers"][name]
+
+									with open('data.json', 'w') as data_file:
+										json.dump(HS_Data, data_file, indent = 4)
+
+									functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully deleted.")
 
 								else:
-									name += i
-								
-								words += 1
-
-							# Checks if the stopwatch exists.
-							if name in HS_Data["stopwatchesAndTimers"]:
-								del HS_Data["stopwatchesAndTimers"][name]
-
-								with open('data.json', 'w') as data_file:
-									json.dump(HS_Data, data_file, indent = 4)
-
-								functions.send_message(recepient_id = data["chatID"], message = f"Stopwatch successfully deleted.")
-
+									functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
+							
 							else:
-								functions.send_message(recepient_id = data["chatID"], message = "Sorry, a stopwatch with that name doesn't exist.")
+								functions.send_message(recepient_id = data["chatID"], message = "Sorry, there seems to have been an error. If you need help with a command call '/help [COMMAND NAME]'.\n\nNote: Dont include the '/' of the command that you need assistance with in your command.")
 
 						delete_stopwatch()
 
