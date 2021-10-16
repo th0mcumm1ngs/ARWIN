c8d7f8a42dcc41d0c96a12b1e3b61e82cae18193
Tom Cummings <rex4343@hotmail.com>
2021-10-16 20:38:24 +0100

    Commands removed from HS_Bot

--- HS_Bot.py
+++ HS_Bot.py
@@ -43,60 +43,11 @@ def start(update, context):
         data = file.read()
     context.bot.send_message(chat_id=update.effective_chat.id, text = data, parse_mode = 'HTML')
 
-def help(update, context):
-    send_request(command = "help", chatID = update.effective_chat.id, args = context.args)
 
-def announce(update, context):
-    send_request(command = "announce", chatID = update.effective_chat.id, args = context.args)
-
-def new_stopwatch(update, context):
-    send_request(command = "newstopwatch", chatID = update.effective_chat.id, args = context.args)
-
-def check_stopwatch(update, context):
-    send_request(command = "checkstopwatch", chatID = update.effective_chat.id, args = context.args)
-
-def reset_stopwatch(update, context):
-    send_request(command = "resetstopwatch", chatID = update.effective_chat.id, args = context.args)
-
-def delete_stopwatch(update, context):
-    send_request(command = "deletestopwatch", chatID = update.effective_chat.id, args = context.args)
-
-# Admin Commands
-
-def power_off_system(update, context):
-    send_request(command = "poweroff", chatID = update.effective_chat.id, args = context.args)
-
-def restart_system(update, context):
-    send_request(command = "restart", chatID = update.effective_chat.id, args = context.args)
 
 # Add the command handlers
-briefing_handler = CommandHandler('announce', announce)
-dispatcher.add_handler(briefing_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
-
-new_stopwatch_handler = CommandHandler('newstopwatch', new_stopwatch)
-dispatcher.add_handler(new_stopwatch_handler)
-
-check_stopwatch_handler = CommandHandler('checkstopwatch', check_stopwatch)
-dispatcher.add_handler(check_stopwatch_handler)
-
-reset_stopwatch_handler = CommandHandler('resetstopwatch', reset_stopwatch)
-dispatcher.add_handler(reset_stopwatch_handler)
-
-delete_stopwatch_handler = CommandHandler('deletestopwatch', delete_stopwatch)
-dispatcher.add_handler(delete_stopwatch_handler)
-
-# Admin Commands
-
-power_off_system_handler = CommandHandler('poweroff', power_off_system)
-dispatcher.add_handler(power_off_system_handler)
-
-restart_system_handler = CommandHandler('restart', restart_system)
-dispatcher.add_handler(restart_system_handler)
 
 updater.start_polling()
