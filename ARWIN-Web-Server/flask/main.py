488cd63452019dccf261fb551b81cf46ca420a01
Tom Cummings <tom.cummings07@outlook.com>
2023-08-02 18:22:55 +0100

    Edit main.py
    • Unnecessary dependencies removed.
    • Server settings JSON file import and usage removed.
    • Secondary route '/home' added to '/'.
    • Comment '# Server Processes' changed to '# API Routes'
    • API Route '/ping' is now restricted to GET requests only and returns "pong", instead of "Connection Successful".
    • Flask server now served natively, without Waitress, on host '0.0.0.0' for uWSGI integration.

--- ARWIN-Web-Server/flask/main.py
+++ ARWIN-Web-Server/flask/main.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template, request
-from waitress import serve
-import datetime, json
+from flask import Flask, render_template
 
-serverSettings = json.load(open('settings.json', 'r'))
-
-serverAddress = serverSettings["SERVER_ADDRESSES"]["web_server"]
-
+# Set up Flask
 app = Flask(__name__)
 
 # Website Routes
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-# Server Processes
+# API Routes
 
-@app.route("/ping")
+@app.route("/ping", methods = ["GET"])
 def ping():
-    return "Connection Succesful"
+    return "pong"
 
 if __name__ == "__main__":
-    serve(app = app, host = serverAddress, port = 8080)
+    app.run(host = '0.0.0.0', debug = False)
+
+# tutorial: https://www.raspberrypi-spy.co.uk/2018/12/running-flask-under-nginx-raspberry-pi/≈
